558be341c68617c78dedb0064c8531df66e870f0
starryeye <see2818@naver.com>
2025-12-05 00:40:46 +0900

    [starryeye, 2025.12.05] - oauth-proxy
    내용
    - oauth-proxy

---
+++ oauth-proxy/src/main/java/com/example/oauth_proxy/OauthProxyApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthProxyApplication.class, args);
+	}
+
+}
---
+++ oauth-proxy/src/test/java/com/example/oauth_proxy/OauthProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
